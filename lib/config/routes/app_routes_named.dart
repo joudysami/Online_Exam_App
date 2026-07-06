1e28590e8460bf456ac646fe926edb7a6f7fb741
joud <joudysami13@gmail.com>
2026-07-06 21:32:49 +0300

    Revert "Merge pull request #1 from joudysami/joudy"
    This reverts commit 7bd9187095bb209bf5b74d407a30d1e7bcef4c79, reversing
    changes made to 9ec961c48fb34047ea66c85ac431f0364c7a2aa7.

--- lib/config/routes/app_routes_named.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutesNamed {
-  static const String login = '/login';
-  static const String signup = '/signup';
-  static const String forgetPassword = '/forget-password';
-  static const String profile = '/profile';
-  static const String home = '/home';
-}
