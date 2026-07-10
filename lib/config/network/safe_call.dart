415a1bf0f911cb923a49f667efc2124b617ff5a6
joud <joudysami13@gmail.com>
2026-07-11 01:59:07 +0300

    first_error_handle

--- lib/config/network/safe_call.dart
+++ lib/config/network/safe_call.dart
@@ -1,1 +1,13 @@
-class SafeCall {}
+import 'package:exam_app/config/base/base_response.dart';
+class SafeCall {
+  Future<BaseResponse<T>> safeApiCall<T>(Future<T> Function() apiCall) async {
+    try {
+      final response = await apiCall();
+      return SuccessResponse(response);
+    } catch (e) {
+      return ErrorResponse(error: e);
+    }
+  }
+}
+//try catch//
+//return safeApiCall(() => api.login());
